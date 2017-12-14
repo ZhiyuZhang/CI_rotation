28d5d1ca8f131e673df3e04a5c52f35aa1913dea
ZhiyuZhang <pmozhang@gmail.com>
2017-12-14 13:14:42 +0100

    first initialisation

--- read.py
+++ read.py
@@ -1,13 +1,15 @@
 import glob, re, os, copy
+
 import numpy as np
-import matplotlib.path
 from   astropy.io import fits
 from   astropy    import wcs
 import astropy.units as u
-from   scipy.interpolate   import UnivariateSpline, SmoothBivariateSpline
 from   astropy.coordinates import ICRS
+
 import matplotlib.pyplot as plt
-import pyfits
+import aplpy
+
+
 
 
 file =  'fitsfile/continuum.fits'
@@ -16,8 +18,12 @@ if os.path.isfile(file) == True:
      image         =  fits.open(file)[0]
      header        =  image.header
      w             =  wcs.WCS(header, image)
-     image         =  image.data
+     image_data    =  image.data
 
 
+plt.clf()
 
-
+fig = plt.figure(figsize                       = (9, 6))
+f   = aplpy.FITSFigure(file,    figure = fig)
+f.show_grayscale(vmin=-0.2E-3, vmax=1E-2,invert=True)
+f.savefig('test.pdf')
