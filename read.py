7057e5dbc83d7f70a59277834fb1fd3e2c349d23
ZhiyuZhang <pmozhang@gmail.com>
2017-12-14 13:23:25 +0100

    first initialisation

--- read.py
+++ read.py
@@ -26,4 +26,17 @@ plt.clf()
 fig = plt.figure(figsize                       = (9, 6))
 f   = aplpy.FITSFigure(file,    figure = fig)
 f.show_grayscale(vmin=-0.2E-3, vmax=1E-2,invert=True)
+
+f.show_contour(file,  levels=[0.0003,0.0004,0.0006,0.0008,0.001,0.002],colors=['red',  'red', 'red', 'red', 'red', 'red'],overlap=True)
+f.set_frame_color('black')
+f.set_tick_labels_font(size='12')
+f.set_axis_labels_font(size='15')
+f.set_tick_labels_format(xformat='hh:mm:ss',yformat='dd:mm:ss')
+f.set_axis_labels(xlabel='R.A. (J2000)', ylabel='Dec. (J2000)', xpad='default', ypad='default')
+f.set_tick_color('black')
+f.show_colorbar()
+fig.text(0.27 ,0.85 ,'test', color='black', size='12')
+
 f.savefig('test.pdf')
+
+
